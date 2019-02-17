b03f9dfdef25627a04c417a03c1911e133760d68
TamizhselvanR <rvtamizhselvan@gmail.com>
2019-02-17 13:19:37 +0530

    Added comments to code

--- leaders-in-an-array.c
+++ leaders-in-an-array.c
@@ -1,8 +1,12 @@
+//Write a program to print all the LEADERS in the array. An element is leader if it is greater than all the elements to its right side.
+
+//And the rightmost element is always a leader. For example int the array {16, 19, 4, 3, 8, 3}, leaders are 19, 8 and 3?
+
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-	int a[10] = {1,18,3,5,2,1},i,n,j;
+	int a[10] = {1,18,5,2,1,6,4,3},i,n,j;
 	n = sizeof(a)/sizeof(int);
 	printf("The leaders are\n");
 	for(i = 0;i < n-1 ;i++)
